f4c20a6c1dac6bde91d1955021265fc6093b0316
Tedokon Ryota <s19014@std.it-college.ac.jp>
2020-05-11 12:36:00 +0900

    5/11 課題 途中

---
+++ chapter01/09.py
@@ -0,0 +1,14 @@
+'''
+スペースで区切られた単語列に対して，各単語の先頭と末尾の文字は残し，それ以外の
+文字の順序をランダムに並び替えるプログラムを作成せよ．ただし，長さが４以下の単
+語は並び替えないこととする．適当な英語の文（例えば”I couldn’t believe that I could
+actually understand what I was reading : the phenomenal power of the human mind .”）
+を与え，その実行結果を確認せよ．
+'''
+
+
+text = 'I couldn’t believe that I could actually understand what I was reading : the phenomenal power of the human mind .'
+
+
+def reordering():
+    pass
